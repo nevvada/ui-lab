9049559e32c91c099a350597f473a6b5ccc0a914
nlemast <nlemaster47@gmail.com>
2021-03-18 14:51:58 -0400

    Implement IntersectionObservation to only fetch imgs once the imgs are in viewport

---
+++ lazyLoadedImages/index.js
@@ -0,0 +1,17 @@
+const renderImage = (entries) => {
+  entries.forEach(entry => {
+    const img = entry.target;
+
+    if (entry.intersectionRatio) {
+      img.src = img.dataset.src;
+    }
+  });
+};
+
+const observer = new IntersectionObserver(renderImage);
+
+const img4 = document.querySelector('#img4');
+const img5 = document.querySelector('#img5');
+const img6 = document.querySelector('#img6');
+
+[img4, img5, img6].forEach(img => observer.observe(img));
