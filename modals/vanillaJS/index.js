a67920f407cdc0d2e16e7379d45b95c6b8f187b0
nlemast <nlemaster47@gmail.com>
2021-03-26 17:48:36 -0400

    Add event listener to overlay and button for displaying modal + overlay and removing it

---
+++ modals/vanillaJS/index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('#openModal');
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+overlay.addEventListener('click', (event) => {
+  const isModalOpen = !modal.classList.contains('hide');
+  const isClickOutsideModal = !modal.contains(event.target);
+
+  if (isModalOpen && isClickOutsideModal) {
+    overlay.classList.add('hide');
+    modal.classList.add('hide');
+  }
+});
+
+button.addEventListener('click', () => {
+  overlay.classList.remove('hide');
+  modal.classList.remove('hide');
+});
